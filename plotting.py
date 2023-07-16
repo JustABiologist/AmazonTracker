12c0d0c4335f0869f91028c5364baa6c9c8ab66a
JustABiologist <florian.gruen@fau.de>
2023-07-16 12:04:40 +0200

    Finally made the first plot available I guess it's getting to the finish line

--- plotting.py
+++ plotting.py
@@ -1,7 +1,9 @@
 import json 
 import matplotlib.pyplot as plt
+import matplotlib.dates as md
 import pandas as pd
 import numpy as np 
+from datetime import datetime
 import os
 
 def readJSONS(json_dir):
@@ -28,21 +30,46 @@ def makeDataframe(json_dicts, filename):
     '''Takes in list of all JSONS in the folder in dict format and returns a list of all the datapoints pertaining to a certain excel file'''
     dicDF = [fileDic[filename] for fileDic in json_dicts]
     links = [list(dic.keys()) for dic in dicDF]
-    links = flatten_concatenation(links)
+    links = list(set(flatten_concatenation(links)))
     values = []
     for dic in dicDF:
         for link in links:
             values.append(dic[link])
     values=flatten_concatenation(values)
     listDF=[]
-    for i,j in zip(links, values):
-        print(j)
-        row = [i, j[3], filename, j[2], j[1]]
+    for j in values:
+        row = [j[0], j[3], filename, j[2], j[1]]
         listDF.append(row)
     df = pd.DataFrame(listDF, columns=["Links", "Timestamp", "Ursprungsexcel", "Preis", "Stückzahl"])
     df_unique = df["Links"].unique().squeeze().tolist()
     df_l = [df[df["Links"] == unique] for unique in df_unique]
     return df_l
+
+def makePlot(dfs):
+    fontdict_xaxis = {'fontsize': 4}
+    
+    xmfd = md.DateFormatter('%d-%m-%y %H Uhr')
+    fig, axs = plt.subplots(2,2, sharex=False, sharey=False)
+    
+    axs[0][0].set_title("Absolute Stückzahl im Lager")
+    axs[0][0].xaxis.set_major_formatter(xmfd)
+    
+    for df in dfs:
+        df = df.sort_values("Timestamp") #Very hacky wont work in August
+        timestapm = [datetime.strptime(time, '%d_%m_%Y %H') for time in df["Timestamp"].values.tolist()]
+        plt.setp(axs, xticks=timestapm, xticklabels=timestapm)
+        axs[0][0].plot(timestapm, df["Stückzahl"], label = df.iloc[0][0])
+        axs[0][0].scatter(timestapm, df["Stückzahl"])
+        axs[0][0].legend()
+        
+        #axs[0][0].set_xticklabels(timestapm, rotation = 25, fontdict = fontdict_xaxis)
+
+    axs[0][1].set_title("Preis gegen Timestamps")
+    axs[1][0].set_title("Verkäufe gegen Timestamps")
+    axs[1][1].set_title("Prozentuales Wachstum Verkäufe")
+    return fig
+
+
 
 #TODO 
 #Make this plottable, and bring in right order/make a df for each diffrent link from this dataframe
@@ -50,7 +77,9 @@ def makeDataframe(json_dicts, filename):
 
 def main():
     jdicts, filenames =  readJSONS("./jsonDUMP/")
-    print(makeDataframe(jdicts, filenames[0])[0])
+    df_l = makeDataframe(jdicts, filenames[0])
+    plot = makePlot(df_l)
+    plt.show()
     return 
 
 if __name__ == "__main__":
