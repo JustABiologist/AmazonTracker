e90fa5d2c7a45fddab28b06941a27324a8c87d26
JustABiologist <florian.gruen@fau.de>
2023-07-17 19:10:48 +0200

    pain at this point tbh

--- Selenium.py
+++ Selenium.py
@@ -29,12 +29,13 @@ def readExcel(Excelpath):
 
 def makeCallStockAmazon(driver_path, link, sleeptime=None):
     
-    chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    service = Service(executable_path=driver_path)
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    #chrome_options = Options()
+    #chrome_options.add_argument('--no-sandbox')
+    #chrome_options.add_argument('--headless')
+    #chrome_options.add_argument('--disable-dev-shm-usage')
+    #service = Service(executable_path=driver_path)
+    #driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome()
     driver.get(link)
     driver.maximize_window()
     # time.sleep(3)
